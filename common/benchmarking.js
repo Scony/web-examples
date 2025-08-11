ead02c02de534da88e5f125600e7f6d112281764
Pawel Lampe <plampe@igalia.com>
2025-08-11 13:47:17 +0200

    Add basic DOM performance demos

---
+++ common/benchmarking.js
@@ -0,0 +1,131 @@
+var Benchmarking = {};
+
+Benchmarking.randomSeed = 49734323;
+Benchmarking.random = Math.random = function() {
+    // Robert Jenkins' 32 bit integer hash function.
+    var randomSeed = Benchmarking.randomSeed;
+    randomSeed = ((randomSeed + 0x7ed55d16) + (randomSeed << 12))  & 0xffffffff;
+    randomSeed = ((randomSeed ^ 0xc761c23c) ^ (randomSeed >>> 19)) & 0xffffffff;
+    randomSeed = ((randomSeed + 0x165667b1) + (randomSeed << 5))   & 0xffffffff;
+    randomSeed = ((randomSeed + 0xd3a2646c) ^ (randomSeed << 9))   & 0xffffffff;
+    randomSeed = ((randomSeed + 0xfd7046c5) + (randomSeed << 3))   & 0xffffffff;
+    randomSeed = ((randomSeed ^ 0xb55a4f09) ^ (randomSeed >>> 16)) & 0xffffffff;
+    Benchmarking.randomSeed = randomSeed;
+    return (randomSeed & 0xfffffff) / 0x10000000;
+}
+
+Benchmarking.now = window.performance && window.performance.now
+                 ? function () { return window.performance.now(); }
+                 : Date.now;
+
+Benchmarking.forceLayout = () => { document.body.offsetWidth; }
+
+Benchmarking.measureRunsPerSecond = function(parameters) {
+    if (!parameters.benchmarkFunction)
+        return;
+
+    if (!parameters.warmupDurationSeconds)
+        parameters.warmupDurationSeconds = 3;
+    if (!parameters.durationSeconds)
+        parameters.durationSeconds = 20;
+
+    if (parameters.blocking)
+        Benchmarking.measureRunsPerSecondBlocking(parameters);
+    else
+        Benchmarking.measureRunsPerSecondNonBlocking(parameters);
+}
+
+// TODO: remove
+Benchmarking.measureRunsPerSecondBlocking = function(parameters) {
+    console.log("Started warmup");
+    setTimeout(() => {
+        const warmupBeginTime = Benchmarking.now();
+        while (true) {
+            parameters.benchmarkFunction();
+            const warmupCurrentTime = Benchmarking.now();
+            if (warmupCurrentTime - warmupBeginTime >= parameters.warmupDurationSeconds * 1000)
+                break;
+        }
+        console.log("Finished warmup");
+        console.log("Started benchmarking");
+        setTimeout(() => {
+            const benchmarkingBeginTime = Benchmarking.now();
+            let runs = 0;
+            while (true) {
+                parameters.benchmarkFunction();
+                ++runs;
+                const benchmarkingCurrentTime = Benchmarking.now();
+                if (benchmarkingCurrentTime - benchmarkingBeginTime >= parameters.durationSeconds * 1000) {
+                    const runsPerSecond = runs / ((benchmarkingCurrentTime - benchmarkingBeginTime) / 1000);
+                    console.log("Finished benchmarking: " + runsPerSecond + " runs/s");
+                    break;
+                }
+            }
+        }, 0);
+    }, 0);
+}
+
+Benchmarking.measureRunsPerSecondNonBlocking = function(parameters) {
+    let measureForASecond = () => {
+        const benchmarkingBeginTime = Benchmarking.now();
+        let runs = 0;
+        while (true) {
+            parameters.benchmarkFunction();
+            ++runs;
+            const benchmarkingCurrentTime = Benchmarking.now();
+            if (benchmarkingCurrentTime - benchmarkingBeginTime >= 1000) {
+                return [runs, benchmarkingCurrentTime - benchmarkingBeginTime];
+            }
+        }
+    }
+
+    let warmupIterationsLeft = parameters.warmupDurationSeconds;
+    let benchmarkingIterationsLeft = parameters.durationSeconds;
+    let totalRuns = 0;
+    let totalTimeDeltaMs = 0;
+
+    console.log("Starting: warmup first, then benchmarking");
+    setTimeout(function iterate() {
+        const [runs, timeDeltaMs] = measureForASecond();
+        if (warmupIterationsLeft-- > 0) {
+            console.log("Warmup iteration " + (parameters.warmupDurationSeconds - warmupIterationsLeft));
+            setTimeout(iterate, 0);
+            return;
+        }
+        totalRuns += runs;
+        totalTimeDeltaMs += timeDeltaMs;
+        const iteration = parameters.durationSeconds - benchmarkingIterationsLeft + 1;
+        const runsPerSecond = totalRuns / (totalTimeDeltaMs / 1000);
+        console.log("Benchmarking iteration " + iteration + ": " + runsPerSecond + " runs/s");
+        if (--benchmarkingIterationsLeft > 0)
+            setTimeout(iterate, 0);
+        else {
+            console.log("Finished benchmarking: " + runsPerSecond + " runs/s");
+            if (parameters.visualResults) {
+                let element = document.createElement("div");
+                element.style.position = 'fixed';
+                element.style.top = '0px';
+                element.style.left = '0px';
+                element.style.backgroundColor = 'orange';
+                element.innerText = "Finished benchmarking: " + runsPerSecond + " runs/s";
+                document.body.appendChild(element);
+            }
+        }
+    }, 0);
+}
+
+Benchmarking.enableFPSLogging = function() {
+    Benchmarking.lastFPSCalculation = Benchmarking.now();
+    Benchmarking.framesSinceLastFPSCalculation = 0;
+    requestAnimationFrame(function loop() {
+        Benchmarking.framesSinceLastFPSCalculation++;
+        var now = Benchmarking.now();
+        var delta = now - Benchmarking.lastFPSCalculation;
+        if (delta > 1000) {
+            console.log("FPS: " + (Benchmarking.framesSinceLastFPSCalculation / (delta / 1000)));
+            Benchmarking.lastFPSCalculation = now;
+            Benchmarking.framesSinceLastFPSCalculation = 0;
+        }
+        requestAnimationFrame(loop);
+    });
+}
